b30763d74a11d16ee0f9edcc2c5e08fb28a879c4
Leonel Gayard <leonel.gayard@gmail.com>
2018-08-19 21:58:50 -0300

    Connect canvas to video

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
+  ngAfterViewInit() {
+    // Grab elements, create settings, etc.
+    const video: HTMLVideoElement   = document.getElementById("myVideo")  as HTMLVideoElement;
+    const canvas: HTMLCanvasElement = document.getElementById("myCanvas") as HTMLCanvasElement;
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+
+    this.startVideoFeed(video);
+
+    const self = this;
+    let i = 0;
+    const snap = function() {
+      // draw image to canvas
+      ctx.drawImage(video, 0, 0, 640, 480);
+      canvas.toBlob(function(blob) {
+        self.upload(blob);
+      }, "image/jpeg", 0.95)
+
+    };
+    const interval = setInterval(snap, 1000);
 
   }
 
+  startVideoFeed(video) {
+    // Get access to the camera!
+    if (!navigator.mediaDevices) {
+      console.log("navigator.mediaDevices: " + navigator.mediaDevices);
+    } else if (!navigator.mediaDevices.getUserMedia) {
+      console.log("navigator.mediaDevices.getUserMedia: " + navigator.mediaDevices.getUserMedia);
+    } else {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        const url = window.URL.createObjectURL(stream);
+        console.log(url);
+        video.src = url;
+        video.play();
+      });
+    }
+  }
+
+  upload(blob) {
+    console.log("upload");
+  }
 }
